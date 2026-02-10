f401af935fc396c82a8e2bab5948489f7d10c04a
Umar <71148652+umrua@users.noreply.github.com>
2026-02-10 22:33:18 +0000

    small change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import umarLogo from './assets/umar.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -15,8 +16,11 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://umaar.netlify.app" target="_blank">
+          <img src={umarLogo} className="logo" alt="Umar logo" />
+        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Umar's Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
